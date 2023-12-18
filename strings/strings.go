4e79acedcc7ad13e6b06f0af1efac7e7c6ca0ce1
Vicky AS <vickyhuang808@gmail.com>
2023-12-18 11:34:06 +0700

    strings

---
+++ strings/strings.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(strings.Contains("Eko Kunthadi", "Eko"))
+	fmt.Println(strings.Split("Kyou fuu Fall Back", " "))
+	fmt.Println(strings.ToLower("ASAS Eko"))
+
+}
